5c6a5b1c14a8cd5b3c01e925121a3c672304c383
Blazing-Jas991 <223303023+Blazing-Jas991@users.noreply.github.com>
2026-04-11 11:25:26 -0700

    medium stage commit

--- 42b7c8cee1e20bc4160b.js
+++ 42b7c8cee1e20bc4160b.js
@@ -1,9 +1,9 @@
 import "./styles.css";
 import { homePage } from './restaurant.js';
 import { menuPage } from "./menu.js";
+import { contactPage } from "./contact.js";
 
 console.log(homePage());
-console.log(menuPage());
 
 import restaurantImage2 from "./restaurant2.jpg";
 const secondImage = document.createElement('img');
@@ -14,13 +14,20 @@ const container = document.getElementById('content');
 const homeButton = document.getElementById('home');
 homeButton.addEventListener('click', () => {
     container.textContent = "";
+    homePage();
 });
 
 const menuButton = document.getElementById('menu');
 menuButton.addEventListener('click', () => {
     container.textContent = "";
+    menuPage();
 });
 
+const contactButton = document.getElementById('contact');
+contactButton.addEventListener('click', () => {
+    container.textContent = "";
+    contactPage();
+});
 
 
 
