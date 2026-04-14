8c7a0e498ebdeff5a8ee6e389b2f479f35adca37
Blazing-Jas991 <223303023+Blazing-Jas991@users.noreply.github.com>
2026-04-14 00:15:42 -0700

    add borders and box-shadows for final edit

---
+++ cf2056efa7263ad084a9.js
@@ -0,0 +1,36 @@
+import "./styles.css";
+import { homePage } from './restaurant.js';
+import { menuPage } from "./menu.js";
+import { contactPage } from "./contact.js";
+
+homePage();
+
+import restaurantImage2 from "./restaurant2.jpg";
+const secondImage = document.createElement('img');
+secondImage.src = restaurantImage2;
+
+const container = document.getElementById('content');
+
+const homeButton = document.getElementById('home');
+homeButton.addEventListener('click', () => {
+    container.textContent = "";
+    homePage();
+});
+
+const menuButton = document.getElementById('menu');
+menuButton.addEventListener('click', () => {
+    container.textContent = "";
+    menuPage();
+});
+
+const contactButton = document.getElementById('contact');
+contactButton.addEventListener('click', () => {
+    container.textContent = "";
+    contactPage();
+});
+
+
+
+
+
+
